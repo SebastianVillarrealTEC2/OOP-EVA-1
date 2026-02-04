4da0ec5c5612321484c71629d10671d8afa6e4d0
SebastianVillarrealTEC2 <L25550588@chihuahua2.tecnm.mx>
2026-02-03 22:20:05 -0600

    Add files via upload

---
+++ EVA1_2_MODIFICADORES_ACCESP/src/main/java/mavenproject2/eva1_2_modificadores_accesp/EVA1_2_MODIFICADORES_ACCESP.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package mavenproject2.eva1_2_modificadores_accesp;
+
+/**
+ *
+ * @author bisonte
+ */
+public class EVA1_2_MODIFICADORES_ACCESP {
+
+    public static void main(String[] args) {
+        Persona persona = new Persona();
+        //persona.   no se ven los atributos privados 
+
+    }
+}
+
+class Persona {
+
+    private String nombre;
+    private String apellidos;
+    private int Edad;
+}
